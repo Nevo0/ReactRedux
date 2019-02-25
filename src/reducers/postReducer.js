5009144366210a3fe6f460db720cfdaa4f65b473
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-25 20:08:18 +0100

    Create Post

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -8,19 +8,11 @@ const inirialState = {
 export default function(state = inirialState, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      // console.log(state);
-      // console.log(action);
-      // console.log(inirialState);
-
       return {
         ...state,
         items: action.payload
       };
     case NEW_POST:
-      // console.log(state);
-      // console.log(action);
-      // console.log(inirialState);
-
       return {
         ...state,
         item: action.payload
